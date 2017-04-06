f58403f3e4c02f3bd60daa415bbbf89c4a9ba14b
Aradi Nóra <aradinora111@gmail.com>
2017-04-06 14:38:04 +0200

    -l lists the tasks

--- todoapp/src/App.java
+++ todoapp/src/App.java
@@ -9,13 +9,30 @@ public class App {
 
   public static void main(String[] args) {
     File usage = new File("assets/usage.csv");
-    try {
-      Scanner scanner = new Scanner(usage);
-      while (scanner.hasNext()) {
-        System.out.println(scanner.nextLine());
+    File tasks = new File("assets/tasks.csv");
+    if (args.length == 0) {
+      try {
+        Scanner usageScanner = new Scanner(usage);
+        while (usageScanner.hasNext()) {
+          System.out.println(usageScanner.nextLine());
+        }
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
       }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
+    } else if (args.length > 0 && args[0].equals("-l")) {
+      try {
+        Scanner taskScanner = new Scanner(tasks);
+        System.out.println();
+        int i = 1;
+        while (taskScanner.hasNext()) {
+          System.out.print(i + " - ");
+          System.out.println(taskScanner.nextLine());
+          i++;
+        }
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }
+
     }
   }
 }
