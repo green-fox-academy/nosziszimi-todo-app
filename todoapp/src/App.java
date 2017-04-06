8a39d87ee55a2af75cedca645d711fc109482b2c
Aradi Nóra <aradinora111@gmail.com>
2017-04-06 16:07:20 +0200

    refactor list and add new task features

--- todoapp/src/App.java
+++ todoapp/src/App.java
@@ -1,12 +1,3 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
 
 /**
  * Created by Nóra on 2017. 04. 06..
@@ -14,35 +5,15 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    Path tasksPath = Paths.get("assets/tasks.csv");
-    File tasks = new File(String.valueOf(tasksPath));
     Usage usage = new Usage();
+    ToDoList todo = new ToDoList();
+
     if (args.length == 0) {
       usage.display();
     } else if (args.length > 0 && args[0].equals("-l")) {
-      try {
-        Scanner taskScanner = new Scanner(tasks);
-        System.out.println();
-        int i = 1;
-        if (!taskScanner.hasNext()) {
-          System.out.println("No todos for today! :)");
-        }
-        while (taskScanner.hasNext()) {
-          System.out.print(i + " - ");
-          System.out.println(taskScanner.nextLine());
-          i++;
-        }
-      } catch (FileNotFoundException e) {
-        e.printStackTrace();
-      }
+      todo.list();
     } else if (args.length > 0 && args[0].equals("-a")) {
-      List<String> newTask = new ArrayList<>();
-      newTask.add(args[1]);
-      try {
-        Files.write(tasksPath, newTask);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+      todo.add(args);
     }
   }
 }
--- todoapp/src/ToDoList.java
+++ todoapp/src/ToDoList.java
@@ -1,6 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by Nóra on 2017. 04. 06..
  */
 public class ToDoList {
+  Path toDoListPath;
+  File toDoList;
 
+  public ToDoList() {
+    toDoListPath = Paths.get("assets/tasks.csv");
+    toDoList = new File(String.valueOf(toDoListPath));
+  }
+
+  void list() {
+    try {
+      Scanner taskScanner = new Scanner(toDoList);
+      System.out.println();
+      int i = 1;
+      if (!taskScanner.hasNext()) {
+        System.out.println("No todos for today! :)");
+      }
+      while (taskScanner.hasNext()) {
+        System.out.print(i + " - ");
+        System.out.println(taskScanner.nextLine());
+        i++;
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  void add(String[] args) {
+    List<String> newTask = new ArrayList<>();
+    newTask.add(args[1]);
+    try {
+      Files.write(toDoListPath, newTask);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
+
