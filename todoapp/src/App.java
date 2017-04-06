2ce29e25d68ece91e0a3f9e811e48251e67aced3
Aradi Nóra <aradinora111@gmail.com>
2017-04-06 15:07:47 +0200

    create feature: add new task with -a

--- todoapp/src/App.java
+++ todoapp/src/App.java
@@ -1,5 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -8,7 +14,9 @@ import java.util.Scanner;
 public class App {
 
   public static void main(String[] args) {
-    File usage = new File("assets/usage.csv");
+    Path usagePath = Paths.get("assets/usage.csv");
+    Path tasksPath = Paths.get("assets/tasks.csv");
+    File usage = new File(String.valueOf(usagePath));
     File tasks = new File("assets/tasks.csv");
     if (args.length == 0) {
       try {
@@ -35,7 +43,14 @@ public class App {
       } catch (FileNotFoundException e) {
         e.printStackTrace();
       }
-
+    } else if (args.length > 0 && args[0].equals("-a")) {
+      List<String> newTask = new ArrayList<String>();
+      newTask.add(args[1]);
+      try {
+        Files.write(tasksPath, newTask);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
